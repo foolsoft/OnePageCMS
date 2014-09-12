7a006935830ce5439122702ea5888c530bde1077
Mamatov Andrey <foolsoft@rambler.ru>
2014-09-12 23:27:51 +0400

    Migration

--- includes/js/initFsCMS.js
+++
@@ -1,7 +0,0 @@
-var URL_ROOT = "http://test.onepagecms.net/ru/";
-var URL_IMG = "http://test.onepagecms.net/includes/img/";
-var URL_JS = "http://test.onepagecms.net/includes/js/";
-var URL_CSS = "http://test.onepagecms.net/includes/css/";
-var URL_SUFFIX = ".asp";
-var LANGUAGE = "ru";
-function T(name){return cmsDictionary[name]==undefined?name:cmsDictionary[name];}
---
+++ kernel/fsCache.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Cache class
+* @package fsKernel
+*/
+class fsCache
+{
+    private $_path = PATH_CACHE;
+    
+    public static function Create()
+    {
+    }
+}
