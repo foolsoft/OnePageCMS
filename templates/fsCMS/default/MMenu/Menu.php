c434b535240ec0f7ab6ce0d77236b4d6fe47a60a
Mamatov Andrey <foolsoft@rambler.ru>
2015-11-21 18:32:39 +0300

    Menu fix
    Menu fix

--- templates/fsCMS/default/MMenu/Menu.php
+++ templates/fsCMS/default/MMenu/Menu.php
@@ -1,7 +1,7 @@
 <ul class="nav navbar-nav <?php echo $tag->class; ?> <?php echo $tag->class; ?>-level-<?php echo $tag->level; ?>" id="menu-level-<?php echo $tag->level; ?>">
     <?php foreach ($tag->items as $itemId => $item) { ?>
         <li class="<?php echo $tag->class; ?>-item <?php echo $tag->class; ?>-item-level-<?php echo $tag->level; ?>">
-            <?php echo fsHtml::Link($item['href'], T($item['title']), '', array('target' => $item['target'])); ?>
+            <?php echo fsHtml::Link($item['href'], T($item['title']), '', array('target' => $item['additional']['target'])); ?>
             {% MMenu/Menu | name=<?php echo $tag->menu; ?>, parent=<?php echo $itemId; ?>, class=<?php echo $tag->class; ?>, level=<?php echo ($tag->level + 1); ?> %}
         </li>
     <?php } ?>
